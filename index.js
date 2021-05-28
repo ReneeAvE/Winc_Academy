f11d2a15b7c60f4b39f0d32ab7384f3758555f80
ReneeAvE <83206352+ReneeAvE@users.noreply.github.com>
2021-05-28 19:45:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Hello Winc Academy");
+let name = "Renée";
+console.log(name);
+
+let getalEen = 4;
+let getalTwee = 4;
+
+console.log(getalEen+getalTwee);
+console.log(getalEen-getalTwee);
+
+let getalDrie = "4";
+let getalVier = "4";
+
+console.log(getalDrie+getalVier);
+
+let age = "vierendertig";
+console.log(typeof age);
+
+
+//Cannot be a reserved keyword
+//Should be meaningful. Omschrijf de variabele. Dus niet a of b.
+//Niet beginnen met cijfer
+//Geen spatie of hyphen (-) dus firstName. Altijd eerst kleine letter en volgende woorden met hoofdletter
+//Hoofdlettergevoelig
+//Multiple variables: komma ertussen, maar good practice is alles op nieuwe regel
