e1dc3cc6f4671dda51a5fcc2d36ec212232533aa
ReneeAvE <83206352+ReneeAvE@users.noreply.github.com>
2021-06-05 16:15:42 +0200

    Add files via upload

---
+++ Opdracht_12/script.js
@@ -0,0 +1,21 @@
+const age = 17;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age >= 18){
+    console.log ("Welkom, kom binnen!");
+} else {
+    console.log ("Sorry, jij moet buiten blijven.");
+}
+
+if (isFemale == true) {
+    console.log ("Jij mag een kaart kopen.");
+} else {
+    console.log ("Jij mag geen kaart kopen");
+}
+
+if (driverStatus =="bob") {
+    console.log ("Wel thuis!");
+} else {
+    console.log ("Blijf jij maar een nachtje hier.");
+}
