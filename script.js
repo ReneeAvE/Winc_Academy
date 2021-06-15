ad6eddf48e43d4284bb1a4cce8bae32bda7c8e8c
ReneeAvE <83206352+ReneeAvE@users.noreply.github.com>
2021-06-15 14:05:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+/*const ageCheck = function (age){
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(ageCheck(21));
+
+const checkAgeAdult = function (age){
+    ageCheck(); {
+        if (age >= 18){
+            return "Hello there";
+        } else {
+            return "Hello kiddo";
+        }
+    }
+}
+console.log(checkAgeAdult(19));
+
+const calculateVat = function (basePrice, vatPercentage){
+    return basePrice * (vatPercentage / 100);
+}
+
+console.log(calculateVat(1000, 21));
+
+const vatCalculator = function (basePrice, vatAmount){
+   const vatAmount = calculateVat();
+   const totalPrice = basePrice + vatAmount;
+   return totalPrice;
+}
+console.log(vatCalculator(1000, 21));
+*/
+
+/* In this exercise we're going to calculate the base price and VAT amount. The main function you make should take the amount including VAT and the VAT percentage. The return value should be an array with two elements: base price and VAT amount. Again: make sure you use two functions, where the main one calls another function to do part of the calculation.
+If you paid 1210 (including VAT) for a computer you can do: - 1210 / 1.21 to get the baseprice - 1210 - (1210 / 1.21) to get the VAT of 210 */
+
+const backCalc = function (amountInclVat, vatPercentage){
+    const amountInclVat = function1();
+    const vatPercentage = ;
+    return basePrice, vatPercentage;
+}
+
+
+//Ok ik snap er helemaal niks meer van. Ik ga eerst door met de volgende opdracht.
+//Voor wie dit leest: excuses dat ik de opdracht niet gemaakt heb maar de cursus zit vol met gaten en ik kom er niet meer uit.
